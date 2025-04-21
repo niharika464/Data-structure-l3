200565c8a856fd76a9796a98706dc331fe8f2e96
niharika464 <niharikasinghgaur11@gmail.com>
2025-04-21 21:45:40 +0530

    arraysum

---
+++ fac2.py
@@ -0,0 +1,6 @@
+def prints(n):
+    if(n<=0):
+        return
+    print("Good Evening...")
+    prints(n/2)
+    prints(n/2)
---
+++ fac3.py
@@ -0,0 +1,8 @@
+def arraysum(a):
+    sum=0
+    for i in a:
+        sum=sum+i
+        return (sum)
+    
+a=[20,8,16,22]
+arraysum(a)    
